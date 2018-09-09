72d7103feb2c7ee68bd21941e36b9d0eb8ab1fe9
Marcin Miler <marcin4951@gmail.com>
2018-09-09 22:13:44 +0200

    Theme and globalStyles

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { ThemeProvider } from 'styled-components'
 
 import { Home } from './Views/Home'
+import { GlobalStyle, theme } from './Theme'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Home />, document.getElementById('root'))
+ReactDOM.render(
+    <ThemeProvider theme={theme}>
+        <React.Fragment>
+            <GlobalStyle />
+            <Home />
+        </React.Fragment>
+    </ThemeProvider>,
+    document.getElementById('root')
+)
 registerServiceWorker()
